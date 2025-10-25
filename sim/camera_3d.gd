ad9837de0837f84c4859ae8625ef71b6cc8954e7
hecarson <hecarson3@gmail.com>
2025-10-24 20:42:42 -0600

    Add mouse capture and capture toggle

--- sim/camera_3d.gd
+++ sim/camera_3d.gd
@@ -1,11 +1,22 @@
 extends Camera3D
 
+var is_mouse_captured = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		print("mouse motion ", event)
+	elif event is InputEventKey and event.keycode == Key.KEY_C and event.pressed:
+		is_mouse_captured = !is_mouse_captured
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	print(is_mouse_captured)
+	if is_mouse_captured:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		
