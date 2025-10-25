06c75e3f7788a635c5b1d724b8a4de099521d8b3
hecarson <hecarson3@gmail.com>
2025-10-24 21:58:05 -0600

    Add camera movement

--- sim/camera_3d.gd
+++ sim/camera_3d.gd
@@ -1,6 +1,8 @@
 extends Camera3D
 
 var is_mouse_captured = true
+var mouse_sens = 0.003
+var move_sens = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,15 +10,32 @@ func _ready() -> void:
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		print("mouse motion ", event)
-	elif event is InputEventKey and event.keycode == Key.KEY_C and event.pressed:
-		is_mouse_captured = !is_mouse_captured
+		rotation.y -= event.relative.x * mouse_sens
+		rotation.x -= event.relative.y * mouse_sens
+	elif event is InputEventKey and event.pressed:
+		if event.keycode == Key.KEY_C:
+			is_mouse_captured = !is_mouse_captured
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(is_mouse_captured)
 	if is_mouse_captured:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	else:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		
+	
+	var velocity = Vector3.ZERO
+	if Input.is_action_pressed("move_forward"):
+		velocity = Vector3.FORWARD
+	elif Input.is_action_pressed("move_back"):
+		velocity = Vector3.BACK
+	elif Input.is_action_pressed("move_left"):
+		velocity = Vector3.LEFT
+	elif Input.is_action_pressed("move_right"):
+		velocity = Vector3.RIGHT
+	elif Input.is_action_pressed("move_up"):
+		velocity = Vector3.UP
+	elif Input.is_action_pressed("move_down"):
+		velocity = Vector3.DOWN
+	velocity *= move_sens
+	translate_object_local(velocity)
