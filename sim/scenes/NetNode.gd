29e999d4321a56b6edcd2555ac5eabb7c374b67a
kiwiDestroyer9000 <cxdvdqdwgn@privaterelay.appleid.com>
2025-10-24 21:07:36 -0600

    six seven

--- sim/scenes/NetNode.gd
+++ sim/scenes/NetNode.gd
@@ -1,11 +1,21 @@
-extends Node2D
+extends Node3D
+class_name NetNode
 
+@export var node_id: String = "node"
+@export var color: Color = Color(0.3, 0.8, 1.0)
 
-# Called when the node enters the scene tree for the first time.
+var neighbors: Dictionary = {}   # neighbor_id -> NetLink
+var is_online: bool = true
+
 func _ready() -> void:
-	pass # Replace with function body.
+	var mat := StandardMaterial3D.new()
+	mat.albedo_color = color
+	mat.emission_enabled = true
+	mat.emission = Color(0.05, 0.7, 0.1)   # default: online glow
+	$MeshInstance3D.material_override = mat
+	$Label3D.text = node_id
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func set_online(state: bool) -> void:
+	is_online = state
+	var mat: StandardMaterial3D = $MeshInstance3D.material_override
+	mat.emission = (Color(0.05, 0.7, 0.1) if state else Color(0.7, 0.1, 0.1))
